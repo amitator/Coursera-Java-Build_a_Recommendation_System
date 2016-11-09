89ce2c906ed0747d38779380cd54c26ce6a0484c
Igor Prus <prus.igor@gmail.com>
2016-11-09 22:19:29 +0300

    edited loadRaters() in FirstRating

--- src/org/coursera/brs/Week_1_Loading_Data/FirstRatings.java
+++ src/org/coursera/brs/Week_1_Loading_Data/FirstRatings.java
@@ -40,7 +40,7 @@ public class FirstRatings {
         System.out.println("\n===================================");
         System.out.println("\t\t\ttestLoadMovies()");
         System.out.println("===================================");
-        System.out.println("Number of movies: " + movies.size());
+        System.out.println("Number of movies: " + movies.size() + "\n");
         int genreCounter = 0;
         int minutesCounter = 0;
         String genre = "Comedy";
@@ -93,14 +93,41 @@ public class FirstRatings {
             String movieID = record.get("movie_id");
             Double rating = Double.parseDouble(record.get("rating"));
             Rater rater = new Rater(raterID);
-            if (!result.contains(rater)) {
+            //Flag turns TRUE if rater was edited and rating was added to that rater
+            boolean flag = false;
+            if (result.size() != 0){
+                //running over all result
+                for (Rater current: result){
+                    //if rater in result then add him more rating and change the flag
+                    if (current.getID().equals(raterID)){
+                        current.addRating(movieID, rating);
+                        flag = true;
+                    }
+                }
+                //if rater not yet in result then add him
+                if (!flag){
+                    rater.addRating(movieID, rating);
+                    result.add(rater);
+                }
+            } else {
+                //for the first record in rating
                 rater.addRating(movieID, rating);
                 result.add(rater);
-            } else {
-                rater.addRating(movieID, rating);
             }
         }
         return result;
+    }
+
+    public void testLoadRaters(){
+        ArrayList<Rater> raters = new ArrayList<>();
+        raters = loadRaters("data\\ratings_short.csv");
+        System.out.println("\n===================================");
+        System.out.println("\t\t\ttestLoadRaters()");
+        System.out.println("===================================");
+        System.out.println("Number of raters: " + raters.size() + "\n");
+        for (Rater current : raters){
+            System.out.println(current.getID() + "\t" + current.getItemsRated());
+        }
     }
 }
 
--- src/org/coursera/brs/Week_1_Loading_Data/Test.java
+++ src/org/coursera/brs/Week_1_Loading_Data/Test.java
@@ -3,6 +3,7 @@ package org.coursera.brs.Week_1_Loading_Data;
 public class Test {
     public static void main(String[] args) {
         FirstRatings firstRatings = new FirstRatings();
-        firstRatings.testLoadMovies();
+//        firstRatings.testLoadMovies();
+        firstRatings.testLoadRaters();
     }
 }
