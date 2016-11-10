b3743a5e582e1c41d8b0e6ff64eacc012b8a7587
Igor Prus <prus.igor@gmail.com>
2016-11-10 13:50:26 +0300

    added getAverageByID() to SecondRating - This method returns a double representing the average movie rating for this ID if there are at least minimalRaters ratings. If there are not minimalRaters ratings, then it returns 0.0.

--- src/org/coursera/brs/SecondRatings.java
+++ src/org/coursera/brs/SecondRatings.java
@@ -31,4 +31,48 @@ public class SecondRatings {
     public int getRaterSize(){
         return myRaters.size();
     }
+
+    public double getAverageByID(String movieID, int minimalRaters){
+        int ratersCounter = 0;
+        double totalRatings = 0.0;
+        for (Rater rater: myRaters){
+            //If rater has rating for movieID movie then ++counter and sum rating to rating counter
+            if (rater.hasRating(movieID)){
+                ratersCounter++;
+                totalRatings+=rater.getRating(movieID);
+            }
+        }
+        if (ratersCounter >= minimalRaters){
+            return totalRatings / (double) ratersCounter;
+        }
+        return 0.0;
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
