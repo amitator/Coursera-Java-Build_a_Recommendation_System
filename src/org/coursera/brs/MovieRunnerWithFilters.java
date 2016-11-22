051006ff75449dfcbb96250d1ad13570e825f2fb
Igor Prus <prus.igor@gmail.com>
2016-11-22 19:48:00 +0300

    added printAverageRatingsByYear() to MovieRunnerWithFilters

--- src/org/coursera/brs/MovieRunnerWithFilters.java
+++ src/org/coursera/brs/MovieRunnerWithFilters.java
@@ -30,9 +30,37 @@ public class MovieRunnerWithFilters {
         ArrayList<Rating> ratings = thirdRatings.getAverageRatings(minNumRatings);
         Collections.sort(ratings);
         System.out.println("Movies that have at least " + minNumRatings + " ratings");
-        System.out.println("Total number of movies " + ratings.size());
+        System.out.println("Total number of movies " + ratings.size() + "\n");
         for (Rating current: ratings){
             System.out.println(current.getValue() + "\t" + MovieDatabase.getTitle(current.getItem()));
+        }
+    }
+
+    /**
+     * This method prints all the movies that have a specified number of minimal ratings and came out in
+     * a specified year or later. Print the number of movies found, and for each movie found,
+     * print its rating, its year, and its title*/
+    public void printAverageRatingsByYear(){
+        MovieDatabase.initialize("ratedmovies_short.csv");
+        System.out.println("\n===================================");
+        System.out.println("\t\t\tprintAverageRatingsByYear()");
+        System.out.println("===================================");
+        ThirdRatings thirdRatings = new ThirdRatings("data\\ratings_short.csv");
+        System.out.println("\nNumber of movies: " + MovieDatabase.size()
+                + "\t" + "Number of ratings: " + thirdRatings.getRaterSize() + "\n");
+        //For all those movies that have at least a specified number of ratings
+        int minNumRatings = 1;
+        int yearAfter = 2000;
+        YearAfterFilter filter = new YearAfterFilter(yearAfter);
+        ArrayList<Rating> ratings = thirdRatings.getAverageRatingsByFilter(minNumRatings, filter);
+        Collections.sort(ratings);
+        System.out.println("Movies that have at least " + minNumRatings + " ratings and came out after"
+                            + yearAfter);
+        System.out.println("Total number of movies " + ratings.size() + "\n");
+        for (Rating current: ratings){
+            System.out.println(current.getValue() + "\t"
+                                + MovieDatabase.getYear(current.getItem()) + "\t"
+                                + MovieDatabase.getTitle(current.getItem()));
         }
     }
 }
