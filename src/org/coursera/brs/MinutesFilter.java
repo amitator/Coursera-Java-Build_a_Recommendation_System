733e07e0bdb0d41e26b3ccbf716874bbd14c019e
Igor Prus <prus.igor@gmail.com>
2016-11-22 21:37:08 +0300

    added MinutesFilter

---
+++ src/org/coursera/brs/MinutesFilter.java
@@ -0,0 +1,26 @@
+package org.coursera.brs;
+
+/**
+ * MinutesFilter that implements Filter.  Its satisfies method should return true
+ * if a movie’s running time is at least min minutes and no more than max minutes.
+ *
+ * @author Igor Prus
+ * @version Nov 22/16
+ *
+ */
+
+public class MinutesFilter implements Filter {
+    private int minTime, maxTime;
+
+    public MinutesFilter(int min, int max){
+        this.minTime = min;
+        this.maxTime = max;
+    }
+
+    @Override
+    public boolean satisfies(String id){
+        return MovieDatabase.getMinutes(id) >= minTime
+                && MovieDatabase.getMinutes(id) < maxTime;
+    }
+
+}
