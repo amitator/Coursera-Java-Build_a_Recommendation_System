80c00a3ff40e671bbdef5e79ac03528e5f862b93
Igor Prus <prus.igor@gmail.com>
2016-11-22 21:20:01 +0300

    added printAverageRatingsByGenre() to MovieRunnerWithFilters

--- src/org/coursera/brs/MovieRunnerWithFilters.java
+++ src/org/coursera/brs/MovieRunnerWithFilters.java
@@ -13,18 +13,22 @@ import java.util.Collections;
  */
 
 public class MovieRunnerWithFilters {
+    private ThirdRatings thirdRatings;
+
+    public MovieRunnerWithFilters(){
+        MovieDatabase.initialize("ratedmovies_short.csv");
+        thirdRatings = new ThirdRatings("data\\ratings_short.csv");
+        System.out.println("\nNumber of movies: " + MovieDatabase.size()
+                + "\t" + "Number of ratings: " + thirdRatings.getRaterSize() + "\n");
+    }
     /**
      * This method prints amount of movies and raitings loaded. Also prints average rating and
      * title for those movies that have at least a specified number of ratings
      */
     public void printAverageRatings(){
-        MovieDatabase.initialize("ratedmovies_short.csv");
         System.out.println("\n===================================");
         System.out.println("\t\t\tprintAverageRatings()");
         System.out.println("===================================");
-        ThirdRatings thirdRatings = new ThirdRatings("data\\ratings_short.csv");
-        System.out.println("\nNumber of movies: " + MovieDatabase.size()
-                + "\t" + "Number of ratings: " + thirdRatings.getRaterSize() + "\n");
         //For all those movies that have at least a specified number of ratings
         int minNumRatings = 1;
         ArrayList<Rating> ratings = thirdRatings.getAverageRatings(minNumRatings);
@@ -39,15 +43,12 @@ public class MovieRunnerWithFilters {
     /**
      * This method prints all the movies that have a specified number of minimal ratings and came out in
      * a specified year or later. Print the number of movies found, and for each movie found,
-     * print its rating, its year, and its title*/
+     * print its rating, its year, and its title
+     */
     public void printAverageRatingsByYear(){
-        MovieDatabase.initialize("ratedmovies_short.csv");
         System.out.println("\n===================================");
         System.out.println("\t\t\tprintAverageRatingsByYear()");
         System.out.println("===================================");
-        ThirdRatings thirdRatings = new ThirdRatings("data\\ratings_short.csv");
-        System.out.println("\nNumber of movies: " + MovieDatabase.size()
-                + "\t" + "Number of ratings: " + thirdRatings.getRaterSize() + "\n");
         //For all those movies that have at least a specified number of ratings
         int minNumRatings = 1;
         int yearAfter = 1990;
@@ -61,6 +62,27 @@ public class MovieRunnerWithFilters {
             System.out.println(current.getValue() + "\t"
                                 + MovieDatabase.getYear(current.getItem()) + "\t"
                                 + MovieDatabase.getTitle(current.getItem()));
+        }
+    }
+
+    /**
+     * This method prints all the movies that have a specified number of minimal ratings and specified genre.
+     * Print the number of movies found, and for each movie found, print its rating, its year, and its title
+     */
+    public void printAverageRatingsByGenre(){
+        System.out.println("\n===================================");
+        System.out.println("\t\t\tprintAverageRatingsByGenre()");
+        System.out.println("===================================");
+        int minNumRatings = 1;
+        String genre = "Crime";
+        GenreFilter genreFilter = new GenreFilter(genre);
+        ArrayList<Rating> ratings = thirdRatings.getAverageRatingsByFilter(minNumRatings, genreFilter);
+        System.out.println("Movies that have at least " + minNumRatings + " ratings and genre "
+                + genre);
+        System.out.println("Total number of movies " + ratings.size() + "\n");
+        for (Rating current: ratings){
+            System.out.println(current.getValue() + "\t"
+                    + MovieDatabase.getTitle(current.getItem()));
         }
     }
 }
