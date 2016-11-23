ae2b130077f52d5ee0cab08a8c998880e37c50fc
Igor Prus <prus.igor@gmail.com>
2016-11-23 18:04:37 +0300

    added printAverageRatingsByYearAfterAndGenre() to MovieRunnerWithFilters

--- src/org/coursera/brs/MovieRunnerWithFilters.java
+++ src/org/coursera/brs/MovieRunnerWithFilters.java
@@ -135,6 +135,39 @@ public class MovieRunnerWithFilters {
             System.out.println("\t\t\t" + MovieDatabase.getDirector(current.getItem()));
         }
     }
+
+    /**
+     * This method calls getAverageRatingsByFilter() to get an ArrayList of type Rating
+     * of all the movies that have a specified number of minimal ratings and the two criteria based
+     * on year and genre. Print the number of movies found, and for each movie, print its rating, its year,
+     * and its title on one line, and all its genres on the next line
+     */
+    public void printAverageRatingsByYearAfterAndGenre(){
+        System.out.println("===================================");
+        System.out.println("printAverageRatingsByYearAfterAndGenre()");
+        System.out.println("===================================");
+        AllFilters allFilters = new AllFilters();
+        //Initializing filters
+        int minNumRatings = 1;
+        int year = 1980;
+        String genre = "Romance";
+        YearAfterFilter yearAfterFilter = new YearAfterFilter(year);
+        GenreFilter genreFilter = new GenreFilter(genre);
+        allFilters.addFilter(yearAfterFilter);
+        allFilters.addFilter(genreFilter);
+        ArrayList<Rating> ratings = thirdRatings.getAverageRatingsByFilter(minNumRatings, allFilters);
+        Collections.sort(ratings);
+        System.out.println("Movies that have at least " + minNumRatings + " ratings of "
+                + genre + " genre, made on or after "
+                + year);
+        System.out.println("Total number of movies " + ratings.size() + "\n");
+        for (Rating current: ratings){
+            System.out.println(current.getValue() + "\t"
+                    + MovieDatabase.getYear(current.getItem()) + "\t"
+                    + MovieDatabase.getTitle(current.getItem()));
+            System.out.println("\t\t\t" + MovieDatabase.getGenres(current.getItem()));
+        }
+    }
 }
 
 
